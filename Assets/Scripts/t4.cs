7100899569bcba33d1bb7c873551218096ad80ce
mackoo13 <kocot@student.agh.edu.pl>
2018-04-28 16:02:28 +0200

    rzeczy

--- Assets/Scripts/t4.cs
+++ Assets/Scripts/t4.cs
@@ -6,19 +6,27 @@ public class t4 : MonoBehaviour {
 	
 	public Texture texture;
 	
-	private string map = "" +
-		"###      #" +
+	private string map1 = "" +
+		"##########" +
+		"# ##    ##" +
+		"# ## ##  #" +
+		"######  ##" +
 		"# #      #" +
-		"#        #" +
-		"###      #" +
-		"  #      #" +
-		"#    #####";
-	private int sizeX = 5;
+		"##########";
+	
+	private string map2 = "" +
+		"##########" +
+		"# ##    ##" +
+		"#    ##  #" +
+		"######  ##" +
+		"#   ######" +
+		"##########";
+		
+	private int sizeX = 10;
 	private int sizeY = 6;
 	private float[] lightX = {1.5f};
 	private float[] lightY = {4.5f};
-	private Vector3 startingPosition = new Vector3(2.5f ,0.5f ,4.5f);
-	private Quaternion startingRotation = new Quaternion(0,0,0, 1);
+	private Quaternion startingRotation = new Quaternion(0,0,0,1);
 	private string playerName = "FPSController";
 	private GameObject player;
 	private float ceilingHeight = 3f;
@@ -31,6 +39,11 @@ public class t4 : MonoBehaviour {
 		return map[y * sizeX + x];
 	}
 	
+	private Vector3 GetCoords(int x, int y, float h) {
+		print(new Vector3(x + .5f, h, y + .5f));
+		return new Vector3(x + .5f, h, y + .5f);
+	}
+	
 	private void PlacePlane(int sizeX, int sizeY, float h, bool isCeiling) {
 		GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
 		plane.transform.position = new Vector3(sizeX / 2f, h, sizeY / 2f);
@@ -38,14 +51,25 @@ public class t4 : MonoBehaviour {
 		plane.transform.localScale = new Vector3(sizeX / 10f, 1f, sizeY / 10f);
 	}
 	
-	private void PlaceTorches() {
-		
+	private void PlaceTorches() {	
 		for(int i=0; i<lightX.Length; i++) {
 			GameObject lightGameObject = new GameObject("The Light");
 			Light lightComp = lightGameObject.AddComponent<Light>();
 			lightComp.color = new Color(.8f, .6f, 0f, 1f);
 			lightGameObject.transform.position = new Vector3(lightX[i], .5f, lightY[i]);
 		}
+	}
+	
+	private void PlaceLabyrinth(string map, int sizeX, int sizeY, float h) {
+		for (int y = 0; y < sizeY; y++) {
+            for (int x = 0; x < sizeX; x++) {
+				if(GetField(map, sizeX, x, y) == '#') {
+					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					cube.transform.position = new Vector3(x + .5f, h + .5f, y + .5f);
+					cube.GetComponent<Renderer>().material.mainTexture = texture;
+				}
+            }
+        }
 	}
 	
 	private void PlacePlayer(Vector3 position, Quaternion rotation){
@@ -68,22 +92,11 @@ public class t4 : MonoBehaviour {
 	
 	void Start () {
 		PlacePlane(sizeX, sizeY, ceilingHeight, true);
-		PlacePlane(sizeX, sizeY, 0f, false);
+		PlacePlane(2*sizeX, 2*sizeY, 0f, false);
 		PlaceTorches();
-				
-        for (int y = 0; y < sizeY; y++) {
-            for (int x = 0; x < sizeX; x++) {
-				if(GetField(map, sizeX, x, y) == '#') {
-					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					cube.AddComponent<Rigidbody>();
-					cube.transform.position = new Vector3(x + .5f, .5f, y + .5f);
-					//cube.transform.localScale = new Vector3(Random.value, Random.value, Random.value);
-					cube.GetComponent<Renderer>().material.mainTexture = texture;
-				}
-            }
-        }
-		
-		PlacePlayer(startingPosition, startingRotation);
+		PlaceLabyrinth(map1, sizeX, sizeY, 0);
+		PlaceLabyrinth(map2, sizeX, sizeY, ceilingHeight - 1);
+		PlacePlayer(GetCoords(1, 1, 64.8f), startingRotation);
 	}
 	
 	void Update () {
