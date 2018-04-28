c1da6ca91fbf5e4fc4642ecfa0d425cafb5150fd
mackoo13 <kocot@student.agh.edu.pl>
2018-04-28 12:01:20 +0200

    rzeczy

---
+++ Assets/Scripts/t4.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t4 : MonoBehaviour {
+	
+	public Texture texture;
+	
+	private string map = "" +
+		"### #" +
+		"# # #" +
+		"#   #" +
+		"### #" +
+		"  #  " +
+		"#   #";
+	
+	private int sizeX = 5;
+	private int sizeY = 6;
+	
+	private char GetField(string map, int sizeX, int x, int y) {
+		return map[y * sizeX + x];
+	}
+	
+	private void PlacePlane(int sizeX, int sizeY, float h, bool isCeiling) {
+		GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		plane.transform.position = new Vector3(sizeX / 2f, h, sizeY / 2f);
+		plane.transform.rotation = Quaternion.Euler(isCeiling ? 180 : 0, 0, 0);
+		plane.transform.localScale = new Vector3(sizeX / 10f, 1f, sizeY / 10f);
+	}
+
+	void Start () {
+		PlacePlane(sizeX, sizeY, 1f, true);
+		PlacePlane(sizeX, sizeY, 0f, false);
+		
+        for (int y = 0; y < sizeY; y++) {
+            for (int x = 0; x < sizeX; x++) {
+				if(GetField(map, sizeX, x, y) == '#') {
+					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					cube.AddComponent<Rigidbody>();
+					cube.transform.position = new Vector3(x + .5f, .5f, y + .5f);
+					//cube.transform.localScale = new Vector3(Random.value, Random.value, Random.value);
+					cube.GetComponent<Renderer>().material.mainTexture = texture;
+				}
+            }
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/t4camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t4camera : MonoBehaviour {
+
+	void Start () {
+		transform.position = new Vector3(2.5f, .5f, 7.5f);
+		transform.rotation = Quaternion.Euler(0, 180, 0);
+	}
+	
+	void Update () {
+		
+	}
+}
