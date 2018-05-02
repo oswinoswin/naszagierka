12597ee9a93109e54a05cee574a0d250b3f0280d
oswin <uchwatm@gmail.com>
2018-05-02 11:13:42 +0200

    Debouncer

--- Assets/Scripts/t4person.cs
+++ Assets/Scripts/t4person.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class t4person : MonoBehaviour {
@@ -7,11 +8,15 @@ public class t4person : MonoBehaviour {
 	private Quaternion startingRotation = new Quaternion(0,0,0,1);
 	private static string playerName = "FPSController";
 	private static GameObject player;
-	public static bool toggleGravity = false;
+	public static bool directionUp = false;
     private static Vector3 down = new Vector3(0.0f, -9.8f, 0f);
     private static Vector3 up = new Vector3(0f, 9.8f, 0f);
 	public static float heightUp = 2.5f;
 	public static float heightDown = 0.5f;
+	DateTime lastAction;
+	DateTime lastPress;
+	TimeSpan waitTime = new TimeSpan(0,0,0,0,300);
+	
 	
 	private Vector3 GetCoords(int x, int y, float h) {
 		print(new Vector3(x + .5f, h, y + .5f));
@@ -24,37 +29,54 @@ public class t4person : MonoBehaviour {
 	}
 	
 	private void MoveUp(){
+		//print("MoveUp");
+		Physics.gravity = up;
 		Vector3 position = player.transform.position;
 		Vector3 newPosition = new Vector3(position[0], heightUp, position[2]);
 		player.transform.position = newPosition;
 	}
 	
 		private void MoveDown(){
+		//print("MoveDown");
+		Physics.gravity = down;
 		Vector3 position = player.transform.position;
-		Vector3 newPosition = new Vector3(position[0], heightUp, position[2]);
+		Vector3 newPosition = new Vector3(position[0], heightDown, position[2]);
 		player.transform.position = newPosition;
 	}
 	
 	private void ToggleGravity(){
-		if (Input.GetKey(KeyCode.G)){
-            toggleGravity = !toggleGravity;
-            //print(Physics.gravity);
-            if (toggleGravity){
-                Physics.gravity = up;
+		if (directionUp){               
 				MoveUp();
             }
-            else {
-                Physics.gravity = down;
-				MoveDown();
-            }
-        } 
+        else{           
+			MoveDown();
+        }
+	}
+	private void CheckForGravityChange(){
+		lastPress = DateTime.Now;
+		TimeSpan delta = lastPress - lastAction;
+		if(delta < waitTime){
+			return;
+		}
+		if (Input.GetKey(KeyCode.G) ){
+			directionUp = !directionUp;
+			print("Change gravity!");
+			lastAction = lastPress;
+		}
 	}
 	
 	void Start () {
 		PlacePlayer(GetCoords(1, 1, 1f), startingRotation);
+		lastAction = DateTime.Now;
+		lastPress = DateTime.Now;
 	}
 	
-	void Update () {
+	void Update () {	
+		CheckForGravityChange();
+		
+	}
+	
+	void FixedUpdate(){
 		ToggleGravity();
 		Vector3 position = player.transform.position;
 		print(position);
